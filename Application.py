812f9babc0e889dc7c5592e2a2f1978b3a738d66
patrick-naylor <patrickknaylor@gmail.com>
2022-10-20 14:58:34 -0700

    Created method to add database to application with custom columns

--- Application.py
+++ Application.py
@@ -1,6 +1,118 @@
 # Written by Patrick Naylor
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtWidgets import *
+from PyQt5.QtSql import *
+import sqlite3
+from preferences import setup
+
 app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
+app.setStyle('Macintosh')
+
+#if(not setup):
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.label = QLabel("Main Window")
+		self.setCentralWidget(self.label)
+
+
+class SetupWindow(QWidget):
+
+	def __init__(self):
+		super().__init__()
+		self.dbw = None
+		layout = QVBoxLayout()
+		self.label = QLabel('Setup Window')
+		layout.addWidget(self.label)
+		self.button = QPushButton("Ready to Track Your Life?")
+		self.button.clicked.connect(self.get_started)
+		layout.addWidget(self.button)
+		self.setLayout(layout)
+
+	def get_started(self, checked):
+		if self.dbw is None:
+			self.dbw = CreateDBWindow()
+		self.dbw.show()
+		self.close()
+
+class CreateDBWindow(QWidget):
+
+	def __init__(self):
+		super().__init__()
+		layout = QVBoxLayout()
+		self.label = QLabel("Create DB Window")
+		layout.addWidget(self.label)
+		self.db = QSqlDatabase.addDatabase('QSQLITE')
+		self.db.setDatabaseName('personal_data.db')
+
+		if not self.db.open():
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Critical)
+			msg.setText('Error in Database Creation')
+			retval = msg.exec_()
+			return False
+		query = QSqlQuery()
+
+		query.exec_('''
+			CREATE TABLE IF NOT EXISTS log
+			(Date DATETIME, Me INTEGER, Day INTEGER)
+			''')
+
+		query.exec_('''
+			CREATE TABLE IF NOT EXISTS variables
+			(Variable TEXT, Goal TEXT)
+			''')
+
+		self.combobox = QComboBox()
+		self.combobox.addItems(['Process Goal', 'Outcome Goal'])
+		layout.addWidget(self.combobox)
+
+		self.textbox = QLineEdit(self)
+		layout.addWidget(self.textbox)
+
+		self.button = QPushButton('Add')
+		self.button.clicked.connect(self.add_column)
+		layout.addWidget(self.button)
+
+		self.doneButton = QPushButton('Done')
+		self.doneButton.clicked.connect(self.close)
+		layout.addWidget(self.doneButton)
+
+		self.setLayout(layout)
+
+	def add_column(self):
+		textboxValue = self.textbox.text()
+		comboboxValue = self.combobox.currentText()
+		query = QSqlQuery()
+		query.exec_(f'''
+			ALTER TABLE log
+			ADD COLUMN {textboxValue} INTEGER''')
+		query = QSqlQuery()
+		query.exec_(f'''
+			INSERT INTO variables (Variable, Goal)
+			VALUES("{textboxValue}", "{comboboxValue}")
+			''')
+
+
+
+
+
+w = MainWindow()
+w.show()
+
+if not setup:
+	sw = SetupWindow()
+	sw.show()
+
+else:
+	w = MainWindow()
+	w.show()
+
+
+
+
+
+
+
+
 app.exec()
