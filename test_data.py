3417d482f06c16f93a73db18fc2eee57e0f3553b
patrick-naylor <patrickknaylor@gmail.com>
2022-10-27 17:57:36 -0700

    Finished code to add test data. Need to add dates

--- test_data.py
+++ test_data.py
@@ -1,14 +1,13 @@
 #Written by Patrick Naylor
-#Generate day data with semirandom cycling
-#Generate 6 other variables with varying levels of corrilation to test data
-#Generate Me data based on test data
-#Turn into pandas dataframe then test_data[n].db
+#Add date data
 
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
 from scipy.optimize import curve_fit
+import random
+import sqlite3
 
 x = np.arange(500) *((2*math.pi)/49) * (500/(365/12))
 r = (np.random.random_sample(500)/2) + .75
@@ -27,8 +26,8 @@ for i in list(np.arange(0, .05, .0001)):
 def func(x, a, b, c):
 	return a * np.exp(-b *x) + c
 popt, pcov = curve_fit(func, p, y, [20, 2130, .1], maxfev=10000)
-print(popt)
-plt.plot(p, y)
+#print(popt)
+#plt.plot(p, y)
 
 
 y_fit = func(p, *popt)
@@ -37,14 +36,44 @@ coeffs_x = []
 for c in coeffs:
 	diff_array = np.absolute(y_fit - c)
 	coeffs_x.append(p[np.argmin(diff_array)])
-print(coeffs_x)
+#print(coeffs_x)
 
 data_arrays = []
+me_data_arrays = []
+scalars = []
 for c_x in coeffs_x:
+	scalar = random.randint(0, 10000)
+	scalars.append(scalar)
 	r1 = (np.random.random_sample(500)*(2*c_x)) + (1-c_x)
 	var = ((np.sin(x*r*r1)+1)/2)
-	print(np.corrcoef(Day, var)[0,1])
-	data_arrays.append(var)
+	#print(np.corrcoef(Day, var)[0,1])
+	me_data_arrays.append(var)
+	data_arrays.append(var*scalar)
+
+#print(np.mean(np.array(data_arrays), axis=0))
+df = pd.DataFrame({
+	'Me': np.mean(np.array(me_data_arrays), axis=0),
+	'Day': Day,
+	'Var1': data_arrays[0],
+	'Var2': data_arrays[1],
+	'Var3': data_arrays[2],
+	'Var4': data_arrays[3],
+	'Var5': data_arrays[4],
+	'Var6': data_arrays[5]
+	})
+
+conn = sqlite3.connect('sample_data.db')
+df.to_sql('log', conn, if_exists='replace', index=False)
+
+df_var = pd.DataFrame({
+	'Variable': ['Var1', 'Var2', 'Var3', 'Var4', 'Var5', 'Var6'],
+	'GoalType': ['Process Goal', 'Process Goal', 'Process Goal', 'Process Goal', 'Process Goal', 'Process Goal'],
+	'Goal': scalars
+})
+df_var.to_sql('variables', conn, if_exists='replace', index=False)
+conn.close()
+
+
 
 
 
